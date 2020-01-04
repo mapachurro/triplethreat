7c8e815c7430dfd7d2570cdda3faa9d98fbd4f08
oliver <oliver.renwick@gmail.com>
2020-01-04 13:04:27 -0500

    ANANAS

---
+++ src/Components/yellowfruit.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './yellowfruit.css';
+
+function Yellowfruit() {
+  return (
+    <div>
+<div class="Alignbox">
+
+  <div class="Alignbox-item">ANANAS</div>
+
+</div>
+  </div>
+  );
+}
+
+export default Yellowfruit;
